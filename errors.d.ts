d74d1902407e32e952f5045a917f2ae0c2ed6950
Meirion Hughes <crakinshot@yahoo.com>
2017-09-10 14:15:11 +0100

    fix: error cause param

--- errors.d.ts
+++ errors.d.ts
@@ -3,12 +3,15 @@ interface InitializationError extends LevelUPError { }
 interface OpenError extends LevelUPError { }
 interface ReadError extends LevelUPError { }
 interface WriteError extends LevelUPError { }
-interface NotFoundError extends Error { }
+interface NotFoundError extends Error {
+  notFound: any;
+  status: any;
+}
 interface EncodingError extends LevelUPError { }
 
 interface LevelUPErrorConstructor<TError> {
-  new(message?: string): TError;
-  (message?: string): TError;
+  new(message?: string, cause?: any): TError;
+  (message?: string, cause?: any): TError;
   readonly prototype: TError;
 }
 
