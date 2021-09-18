3576ead7edf7e2597269b26c18a03f4914be1a5a
Vincent Weevers <mail@vincentweevers.nl>
2021-09-18 15:41:34 +0200

    Inline `errno` package

--- errors.js
+++ errors.js
@@ -1,11 +1,34 @@
 'use strict'
 
-const createError = require('errno').create
-const LevelUPError = createError('LevelUPError')
-const NotFoundError = createError('NotFoundError', LevelUPError)
+function createError (type, Proto) {
+  const Err = function (message, cause) {
+    if (message && typeof message !== 'string') {
+      message = message.message || message.name
+    }
 
-NotFoundError.prototype.notFound = true
-NotFoundError.prototype.status = 404
+    // can be passed just a 'cause'
+    cause = typeof message !== 'string' ? message : cause
+
+    Object.defineProperty(this, 'type', { value: type, enumerable: false, writable: false, configurable: false })
+    Object.defineProperty(this, 'name', { value: type, enumerable: false, writable: false, configurable: false })
+    Object.defineProperty(this, 'cause', { value: cause, enumerable: false, writable: false, configurable: false })
+    Object.defineProperty(this, 'message', { value: message, enumerable: false, writable: false, configurable: false })
+
+    Error.call(this)
+
+    if (typeof Error.captureStackTrace === 'function') {
+      Error.captureStackTrace(this, Err)
+    }
+  }
+
+  if (Proto != null) {
+    Err.prototype = new Proto()
+  }
+
+  return Err
+}
+
+const LevelUPError = createError('LevelUPError')
 
 module.exports = {
   LevelUPError: LevelUPError,
@@ -13,6 +36,9 @@ module.exports = {
   OpenError: createError('OpenError', LevelUPError),
   ReadError: createError('ReadError', LevelUPError),
   WriteError: createError('WriteError', LevelUPError),
-  NotFoundError: NotFoundError,
+  NotFoundError: createError('NotFoundError', LevelUPError),
   EncodingError: createError('EncodingError', LevelUPError)
 }
+
+module.exports.NotFoundError.prototype.notFound = true
+module.exports.NotFoundError.prototype.status = 404
